0fb59837d6984786701f0f4213a353f3f12870fb
burntcheeto15 <ewolter31@gmail.com>
2024-10-02 15:06:06 -0500

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Reflection;
+
+namespace Midterm_Project
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private string genre;
+        private int pageLength;
+        private int yearPublished;
+        private bool checkedOut;
+        public Book(string title, string author, string genre, int pageLength, int yearPublished, bool checkedOut)
+        {
+            title = Title;
+            author = Author;
+            genre = Genre;
+            pageLength = PageLength;
+            yearPublished = YearPublished;
+            checkedOut = CheckedOut;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} by {Author} ({YearPublished}) | Genre: {Genre} Page Count: {PageLength} | Checked Out: {CheckedOut}";
+        }
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Title cannot be null or empty");
+                }
+                title = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Author's Name cannot be null or empty");
+                }
+                author = value;
+            }
+        }
+
+        public string Genre
+        {
+            get
+            {
+                return genre;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Genre cannot be null or empty");
+                }
+                genre = value;
+            }
+        }
+
+        public int PageLength
+        {
+            get
+            {
+                return pageLength;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Page Number cannot be 0 or less");
+                }
+                pageLength = value;
+            }
+        }
+
+        public int YearPublished
+        {
+            get
+            {
+                return yearPublished;
+            }
+            set
+            {
+                if (value <= 1455 || value > DateTime.Now.Year)
+                {
+                    throw new Exception("Year cannot be before 868 or in the future");
+                }
+                yearPublished = value;
+            }
+        }
+
+        public bool CheckedOut
+        {
+            get
+            {
+                return checkedOut;
+            }
+            set
+            {
+                checkedOut = value;
+            }
+        }
+    }
+}
---
+++ Library.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Midterm_Project
+{
+    public class Library
+    {
+        
+        private static void Main()
+        {
+
+
+            const string file = "Library Books.csv";
+            if (!File.Exists(file))
+            {
+                Console.WriteLine("File doesn't Exist");
+            }
+            int lineCount = GetLineCount(file);
+
+            Book[] library = new Book[lineCount - 1];
+            try
+            {
+                using StreamReader reader = new StreamReader(file);
+                reader.ReadLine();
+                for (int i = 0; i < lineCount; i++)
+                {
+                    string line = reader.ReadLine();
+                    string[] col = line.Split(",");
+
+                    string author = col[0];
+                    string title = col[1];
+                    int yearPublished = int.Parse(col[2]);
+                    string genre = col[3];
+                    int pageLength = int.Parse(col[4]);
+                    bool checkedOut = bool.Parse(col[5]);
+
+                    library[i] = new Book(title, author, genre, pageLength, yearPublished, checkedOut);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error reading");
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("Books in the Library");
+            for (int i = 0; i < lineCount - 1; i++)
+            {
+                Console.WriteLine(library[i]);
+            }
+        }
+
+        private static int GetLineCount(string file)
+        {
+            using StreamReader reader = new StreamReader(file);
+            int lines = 0;
+            while (!reader.EndOfStream)
+            {
+                reader.ReadLine();
+                lines++;
+            }
+            return lines;
+        }
+
+
+    }
+}
